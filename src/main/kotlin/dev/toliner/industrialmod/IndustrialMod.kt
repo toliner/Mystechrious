3144b0e674d8dc7c0fc1e95e35ea19e4947cb403
kobi32768 <40712426+kobi32768@users.noreply.github.com>
2020-08-02 12:09:32 +0900

    Merge pull request #1
    * Add itemGroup

--- src/main/kotlin/dev/toliner/industrialmod/IndustrialMod.kt
+++ src/main/kotlin/dev/toliner/industrialmod/IndustrialMod.kt
@@ -1,5 +1,8 @@
 package dev.toliner.industrialmod
 
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.toliner.korgelin.api.KotlinMod
 import net.toliner.korgelin.container.KotlinModLoadingContext
 import org.apache.logging.log4j.LogManager
@@ -14,5 +17,9 @@ object IndustrialMod {
         KotlinModLoadingContext.get().eventBus.apply {
 
         }
+    }
+
+    val itemGroup: ItemGroup = object : ItemGroup(modId) {
+        override fun createIcon(): ItemStack { return ItemStack(Items.DIAMOND) }
     }
 }
